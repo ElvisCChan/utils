bffd54add98923f8396b062a5ef4e2857479e513
ElvisCChan <66022475+ElvisCChan@users.noreply.github.com>
2023-05-05 00:35:11 +0800

    initial commit

---
+++ utils-api/src/main/java/com/github/elviscchan/utils/utilsapi/UtilsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.elviscchan.utils.utilsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilsApiApplication.class, args);
+	}
+
+}
---
+++ utils-api/src/test/java/com/github/elviscchan/utils/utilsapi/UtilsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elviscchan.utils.utilsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
